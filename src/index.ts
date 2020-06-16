690ab02d8f07c239f37fd44dc053c4e5e0d966e0
Spencer Sharkey <spencer@sf-n.com>
2020-06-16 17:51:41 +0000

    ts 3.9.5

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,40 @@
-import { Registry, StackElement, INITIAL } from 'monaco-textmate'
-import * as monacoNsps from 'monaco-editor'
-import { TMToMonacoToken } from './tm-to-monaco-token';
+import * as monacoNsps from '@uplol/monaco-editor'
+import {INITIAL, Registry, StackElement} from 'monaco-textmate'
+
+import {TMToMonacoToken} from './tm-to-monaco-token';
 
 class TokenizerState implements monacoNsps.languages.IState {
+  constructor(private _ruleStack: StackElement) {}
 
-    constructor(
-        private _ruleStack: StackElement
-    ) { }
+  public get ruleStack(): StackElement {
+    return this._ruleStack
+  }
 
-    public get ruleStack(): StackElement {
-        return this._ruleStack
+  public clone(): TokenizerState {
+    return new TokenizerState(this._ruleStack);
+  }
+
+  public equals(other: monacoNsps.languages.IState): boolean {
+    if (!other || !(other instanceof TokenizerState) || other !== this ||
+        other._ruleStack !== this._ruleStack) {
+      return false;
     }
-
-    public clone(): TokenizerState {
-        return new TokenizerState(this._ruleStack);
-    }
-
-    public equals(other: monacoNsps.languages.IState): boolean {
-        if (!other ||
-            !(other instanceof TokenizerState) ||
-            other !== this ||
-            other._ruleStack !== this._ruleStack
-        ) {
-            return false;
-        }
-        return true;
-    }
+    return true;
+  }
 }
 
 /**
  * Wires up monaco-editor with monaco-textmate
  *
- * @param monaco monaco namespace this operation should apply to (usually the `monaco` global unless you have some other setup)
- * @param registry TmGrammar `Registry` this wiring should rely on to provide the grammars
+ * @param monaco monaco namespace this operation should apply to (usually the
+ * `monaco` global unless you have some other setup)
+ * @param registry TmGrammar `Registry` this wiring should rely on to provide
+ * the grammars
  * @param languages `Map` of language ids (string) to TM names (string)
  */
-export function wireTmGrammars(monaco: typeof monacoNsps, registry: Registry, languages: Map<string, string>, editor?: monacoNsps.editor.ICodeEditor) {
+export function wireTmGrammars(
+    monaco: typeof monacoNsps, registry: Registry,
+    languages: Map<string, string>, editor?: monacoNsps.editor.ICodeEditor) {
     return Promise.all(
         Array.from(languages.keys())
             .map(async (languageId) => {
@@ -43,15 +42,18 @@ export function wireTmGrammars(monaco: typeof monacoNsps, registry: Registry, la
                 monaco.languages.setTokensProvider(languageId, {
                     getInitialState: () => new TokenizerState(INITIAL),
                     tokenize: (line: string, state: TokenizerState) => {
-                        const res = grammar.tokenizeLine(line, state.ruleStack)
-                        return {
-                            endState: new TokenizerState(res.ruleStack),
-                            tokens: res.tokens.map(token => ({
-                                ...token,
-                                // TODO: At the moment, monaco-editor doesn't seem to accept array of scopes
-                                scopes: editor ? TMToMonacoToken(editor, token.scopes) : token.scopes[token.scopes.length - 1]
-                            })),
-                        }
+    const res = grammar.tokenizeLine(line, state.ruleStack)
+    return {
+      endState: new TokenizerState(res.ruleStack),
+          tokens: res.tokens.map(
+              token => ({
+                ...token,
+                // TODO: At the moment, monaco-editor doesn't seem to accept
+                // array of scopes
+                scopes: editor ? TMToMonacoToken(editor, token.scopes) :
+                                 token.scopes[token.scopes.length - 1]
+              })),
+    }
                     }
                 })
             })
